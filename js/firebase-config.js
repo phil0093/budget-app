c152083b244f75defa36fb4596654b1d29125ebf
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-08 15:32:41 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,6 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
-import {
-    getFirestore
-} from
-"https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA2MRLuuhnYaB8xe7VoHh4koTt0UeU6kcU",
@@ -14,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "311289372367",
   appId: "1:311289372367:web:baa3d20ce527dfc645e1ba"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
