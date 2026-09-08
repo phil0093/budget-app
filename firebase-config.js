c1d992fd38cd7b6a20091cc046d3c311e637d8b2
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-08 15:10:15 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,20 +7,16 @@ import {
 "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const firebaseConfig = {
-
-    apiKey: "XXX",
-
-    authDomain: "XXX",
-
-    projectId: "XXX",
-
-    storageBucket: "XXX",
-
-    messagingSenderId: "XXX",
-
-    appId: "XXX"
+  apiKey: "AIzaSyA2MRLuuhnYaB8xe7VoHh4koTt0UeU6kcU",
+  authDomain: "budget-metp.firebaseapp.com",
+  projectId: "budget-metp",
+  storageBucket: "budget-metp.firebasestorage.app",
+  messagingSenderId: "311289372367",
+  appId: "1:311289372367:web:baa3d20ce527dfc645e1ba"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export { app };
