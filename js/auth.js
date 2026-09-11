a062d0f046992d4c47ca2ede7c6d628b8e43b104
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-11 18:08:50 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -16,7 +16,7 @@ const provider = new GoogleAuthProvider();
 
 // Remplace ces adresses par les vraies
 const utilisateursAutorises = [
-    "philippefrossard0093@hotmail.fr",
+    "philippefrossard0093@gmail.com",
     "marion.gathelier@laposte.net"
 ];
 
