ce90ff54ee455929e11bd3954d46633e6d331a57
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-18 11:37:43 +0200

    Update mois.js

--- js/mois.js
+++ js/mois.js
@@ -3,9 +3,27 @@ import { db } from "./firebase-config.js";
 import {
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    addDoc
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
+export async function ajouterMouvementMois(
+    mois,
+    mouvement
+) {
+
+    await addDoc(
+        collection(
+            db,
+            "mois",
+            mois,
+            "mouvements"
+        ),
+        mouvement
+    );
+
+}
 export async function moisExiste(mois) {
 
     const snapshot = await getDoc(
