15140c696a53631c2aad2b27edc4420006e98292
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-22 13:21:43 +0200

    Create courses.js

---
+++ js/courses.js
@@ -0,0 +1,46 @@
+import { db } from "./firebase-config.js";
+
+import {
+    collection,
+    getDocs,
+    doc,
+    setDoc
+} from
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+export async function chargerListesCourses() {
+
+    const snapshot =
+        await getDocs(
+            collection(
+                db,
+                "listesCourses"
+            )
+        );
+
+    return snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+
+}
+
+export async function sauvegarderListeCourses(
+    nom,
+    lignes
+) {
+
+    await setDoc(
+        doc(
+            db,
+            "listesCourses",
+            nom
+        ),
+        {
+            nom,
+            lignes
+        }
+    );
+
+}
+
