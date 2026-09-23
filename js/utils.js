c9ba7ad942b1b19c8fb163d1da10a00525b3eba3
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-23 10:23:15 +0200

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,18 @@
+export function formatDateLocale(date) {
+
+    const annee =
+        date.getFullYear();
+
+    const mois =
+        String(
+            date.getMonth() + 1
+        ).padStart(2, "0");
+
+    const jour =
+        String(
+            date.getDate()
+        ).padStart(2, "0");
+
+    return `${annee}-${mois}-${jour}`;
+
+}
