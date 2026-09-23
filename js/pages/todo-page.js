ad971470b3c244fe1eb339d4b40c61bd7263ae58
phil0093 <philippefrossard0093@hotmail.fr>
2026-09-23 11:55:12 +0200

    Update todo-page.js

--- js/pages/todo-page.js
+++ js/pages/todo-page.js
@@ -1,14 +1,53 @@
 import {
     chargerTodo,
     ajouterTodo,
-    supprimerTodo
+    supprimerTodo,
+    modifierTodo
 }
 from "../services/todo.js";
 
+async function nettoyerTodosAnciennes() {
+
+    const maintenant =
+        Date.now();
+
+    const deuxJours =
+        2 * 24 * 60 * 60 * 1000;
+
+    const todos =
+        await chargerTodo();
+
+    for (const todo of todos) {
+
+        if (!todo.dateTerminee) {
+            continue;
+        }
+
+        const dateTerminee =
+            new Date(
+                todo.dateTerminee
+            ).getTime();
+
+        if (
+            maintenant - dateTerminee >
+            deuxJours
+        ) {
+
+            await supprimerTodo(
+                todo.id
+            );
+
+        }
+
+    }
+
+}
 
 window.afficherTodo =
 async function() {
 
+    await nettoyerTodosAnciennes();
+    
     document
         .getElementById("sidebar")
         .classList
@@ -24,6 +63,18 @@ async function() {
 
     const todos =
         await chargerTodo();
+    
+    todos.sort((a, b) => {
+    
+        if (
+            a.terminee === b.terminee
+        ) {
+            return 0;
+        }
+    
+        return a.terminee ? 1 : -1;
+    
+    });
 
     const lignes =
         todos.map(t => `
@@ -32,16 +83,24 @@ async function() {
 
                 <input
                     type="checkbox"
+                    ${t.terminee ? "checked" : ""}
                     onchange="
-                        terminerTodo(
+                        toggleTodo(
                             '${t.id}'
                         )
-                    ">
-
-                <span>
+                    "
+                >
+            
+                <span
+                    class="${
+                        t.terminee
+                        ? "todoTermine"
+                        : ""
+                    }"
+                >
                     ${t.texte}
                 </span>
-
+            
             </div>
 
         `).join("");
@@ -100,11 +159,33 @@ async function(event) {
 
 };
 
-window.terminerTodo =
+window.toggleTodo =
 async function(id) {
 
-    await supprimerTodo(
-        id
+    const todos =
+        await chargerTodo();
+
+    const todo =
+        todos.find(
+            t => t.id === id
+        );
+
+    if (!todo) {
+        return;
+    }
+
+    const terminee =
+        !todo.terminee;
+
+    await modifierTodo(
+        id,
+        {
+            terminee,
+            dateTerminee:
+                terminee
+                ? new Date().toISOString()
+                : null
+        }
     );
 
     await afficherTodo();
